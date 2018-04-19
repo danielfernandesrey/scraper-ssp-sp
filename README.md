add6ad35adecf4f5672a40ceff59eab091dbfdca
Daniel <danielrcomp@gmail.com>
2018-04-19 10:32:12 -0300

    Adding description to the README.md file and removing prints from the main file.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scraper-ssp-sp
-Scraper for the SSP-SP site
+Scraper for the SSP-SP site.
+
+This project was created to help download a xls file with data from the SSP-SP site in an easy and clean way.
+Since the SSP-SP site is a government site, the idea was to download a xls file instead of extract html tables from all the pages
+so we wouldn't hit the server to hard.
+
+
+
--- main.py
+++ main.py
@@ -4,18 +4,20 @@ Created on Tue Apr 17 22:15:23 2018
 
 @author: daniel
 """
+import re
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_viewstate(html):
-    
-    soup = BeautifulSoup(html, 'lxml')    
+    soup = BeautifulSoup(html, 'lxml')
     viewstate = soup.find('input', attrs={'id': '__VIEWSTATE'})
     viewstate_value = viewstate['value']
     eventvalidation = soup.find('input', attrs={'id': '__EVENTVALIDATION'})
     eventvalidation_value = eventvalidation['value']
-    
+
     return viewstate_value, eventvalidation_value
+
 
 headers = {
     'Origin': 'http://www.ssp.sp.gov.br',
@@ -30,53 +32,49 @@ headers = {
     'Connection': 'keep-alive',
 }
 
+
 def get_html(session, viewstate, event_validation, event_target, outro=None, stream=False, hdfExport=''):
-    
     url = "http://www.ssp.sp.gov.br/transparenciassp/"
     data = [
-            ('__EVENTTARGET', event_target),
-            ('__EVENTARGUMENT', ''),
-            ('__VIEWSTATE', viewstate),
-            ('__EVENTVALIDATION', event_validation),
-            ('ctl00$cphBody$hdfExport', hdfExport),
-            
-          ]
-    
+        ('__EVENTTARGET', event_target),
+        ('__EVENTARGUMENT', ''),
+        ('__VIEWSTATE', viewstate),
+        ('__EVENTVALIDATION', event_validation),
+        ('ctl00$cphBody$hdfExport', hdfExport),
+
+    ]
+
     if outro:
         data.append(('ctl00$cphBody$filtroDepartamento', '0'))
         data.append(('__LASTFOCUS', ''))
-    
+
     response = session.post(url, headers=headers, data=data, stream=stream)
     return response
 
+
 def download():
+    session = requests.session()
 
-	session = requests.session()
+    url = "http://www.ssp.sp.gov.br/transparenciassp/"
 
-	url = "http://www.ssp.sp.gov.br/transparenciassp/"
+    response = session.post(url, headers=headers)
+    viewstate, eventvalidation = get_viewstate(response.text)
 
-	response = session.post(url, headers=headers)
-	viewstate, eventvalidation = get_viewstate(response.text)	
-
-	parametros = [
-					 ['ctl00$cphBody$btnRouboCelular'],
-					 ['ctl00$cphBody$lkAno16', True, False],
-					 ['ctl00$cphBody$lkMes2', True, False],
-					 ['ctl00$cphBody$ExportarBOLink', True, True, 0]
-				  ]
-	for i in range(len(parametros)-1):
-		print(parametros[i])
-		response = get_html(session, viewstate, eventvalidation, *parametros[i])
-		html = response.text
-		viewstate, eventvalidation = get_viewstate(html)
-
-	response = get_html(session, viewstate, eventvalidation, *parametros[-1])
-	try:
-		print(response.headers['attachment'])
-	except Exception:
-		pass
-	with open('t.xls', 'wb') as f:
-		f.write(response.text.encode('utf-8'))
-
-
-
+    parametros = [
+        ['ctl00$cphBody$btnRouboCelular'],
+        ['ctl00$cphBody$lkAno17', True, False],
+        ['ctl00$cphBody$lkMes10', True, False],
+        ['ctl00$cphBody$ExportarBOLink', True, True, 0]
+    ]
+    for i in range(len(parametros) - 1):
+        response = get_html(session, viewstate, eventvalidation, *parametros[i])
+        html = response.text
+        viewstate, eventvalidation = get_viewstate(html)
+    response = get_html(session, viewstate, eventvalidation, *parametros[-1])
+    try:
+        nome = re.search('=.*xls', response.headers['content-disposition'])
+        nome = nome.group().replace('=', '')
+    except Exception:
+        nome = "dados.xls"
+    with open(nome, 'wb') as f:
+        f.write(response.content)
